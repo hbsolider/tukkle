cc65163a7560baef74d8a4050d5c4ea3cde4e17a
hoaibao <rorolo2011@gmail.com>
2020-09-12 11:51:24 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App" style={{display:'flex',alignItems:'center',justifyContent:'center',minHeight:'100vh'}}>
+      
+    </div>
+  );
+}
+export default App;
